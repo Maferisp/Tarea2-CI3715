6ba0cb33ccba92ff764cc6566b5f85a6e551f41c
mafer <mafermach@gmail.com>
2020-01-29 15:28:20 -0400

    agregar restricciones para el calculo de las mujeres, if name == main

--- Pension.py
+++ Pension.py
@@ -3,6 +3,9 @@ from datetime import date
 from datetime import timedelta
 
 def verification (age, sex, situation, SDate):
+    # Fecha actual
+    actual = date.today()
+
     paid = (actual - SDate).days
     if (paid >= 750*5):
         if(sex == 1): # Los masculinos
@@ -24,29 +27,25 @@ def verification (age, sex, situation, SDate):
             elif(situation == 1): # Situacion insalubre
                 Fnumber = (paid/365) // 4 # Numero de anios en los que trabaja insalubre
                 if(Fnumber >= 1 and Fnumber <= 5): # El numero de anios permitido
-                    if(age >= 55 - Fnumber):
-                        return True
-                    else:
-                        return False
+                    return age >= 55 - Fnumber
+                elif (Fnumber > 5):
+                    return age >= 50
             elif(situation == 0 and age < 55): # Situacion salubre y fuera del rango debido
                 return False
     else:
         return False
 
 
+if __name__ == '__main__':
 
+    # Datos par la fecha de inicio de trabajo
+    year = int(input("Anyo de 4 cifras: "))
+    month = int(input("Mes: "))
+    day = int(input("Dia: "))
+    SDate = date(year, month, day)
 
-# Fecha actual
-actual = date.today()
-
-# Datos par la fecha de inicio de trabajo
-year = int(input("Anyo de 4 cifras: "))
-month = int(input("Mes: "))
-day = int(input("Dia: "))
-SDate = date(year, month, day)
-
-# Datos para la verificacion de pension
-age = int(input("Age: "))
-sex = int(input("Sexo(1 = Masculino, 0 = Femenino): "))
-situation = int(input("¿Considera que trabaja bajo condiciones salubres(1) o insalubres(0)?: "))
-verification(age, sex, situation, SDate)
+    # Datos para la verificacion de pension
+    age = int(input("Age: "))
+    sex = int(input("Sexo(1 = Masculino, 0 = Femenino): "))
+    situation = int(input("¿Considera que trabaja bajo condiciones salubres(1) o insalubres(0)?: "))
+    verification(age, sex, situation, SDate)
